05617d2b5ef946f7ce82139ffb6ed7802c42650d
David Stern <sternd@janelia.hhmi.org>
2014-10-02 16:56:30 -0400

    update

--- errorbarjitter.m
+++ errorbarjitter.m
@@ -3,10 +3,10 @@ function errorbarjitter(data,h,varargin)
 % SYNTAX
 %
 %   errorbarjitter(data)
-%   errorbarjitter(data,param1,val1,param2,val2,...)
+%   errorbarjitter(data,h,param1,val1,param2,val2,...)
 %
 %   e.g.
-%   errorbarjitter(data,'barends','yes','colors',color_array)
+%   errorbarjitter(data,1,'barends','yes','colors',color_array)
 %
 %
 % Plots mean (or median)±SD plus jitter plot of raw data
@@ -19,47 +19,48 @@ function errorbarjitter(data,h,varargin)
 % AVERAGE: plot with either mean ('mean') or median ('median') or none
 %   ('none')
 %
-% SORT: plot with either sorted data ('sort') or user entered 
+% SORT: plot with either sorted data ('sort') or user entered
 %   order ('nosort'). Default is nosort.
 %
-% COLHEADERS: plot with user defined column headers. 
+% COLHEADERS: A cell array containing user defined column headers.
 %   It is a bad idea to sort without providing colheaders,
-%   since it may not be easy to track the source of the data. 
+%   since it may not be easy to track the source of the data.
 %
 % FACTOR: plot with user defined factor for scaling the jitter
 %
-% LEFT_OR_RIGHT: when LEFT_OR_RIGHT = 'left', plot with mean±SD line 
+% LEFT_OR_RIGHT: when LEFT_OR_RIGHT = 'left', plot with mean±SD line
 %   on left, when = 'right', plot with bar on right (default)
 %
 % BARENDS: when barends = 'yes', plot with capped ends of SD
 %   bars, when = 'no' (default), plot without barends
 %
-% STD: Provide a separate array containing the standard deviations
-%   (or any other estimates of distribution) for each datum in d, and an error
-%   bar equal to ± these deviations will be plotted on each datum
+% STD: An array containing the standard deviations (or any other estimates
+%   of distribution) for each datum in d, and an error bar equal to ± these
+%   deviations will be plotted on each datum
 %
-% COLORS: Plot data with user defined colors, using standard Matlab nomenclature, 
-%   specified as cell array equal in length to the number of data columns.
+% COLORS: Cell array equal in length to the number of data columns
+%   containing user defined colors, using standard Matlab nomenclature
 %
-% COLOR_OPTS: A three element logical vector that indicates whether to 
-%   color the raw data (position 1), the average point (position 2), and/or 
-%   the bar representing the standard deviation estimate (position 3). e.g. [0 1 1] 
+% COLOR_OPTS: A three element logical vector that indicates whether to
+%   color the raw data (position 1), the average point (position 2), and/or
+%   the bar representing the standard deviation estimate (position 3). e.g. [0 1 1]
 %   would color the average point and the variance line
 %
-% COLOR_MAP: Provide a matrix equal in dimension to the data matrix to plot
+% COLOR_MAP: A matrix equal in dimension to the data matrix to plot
 %   each datum a color from the scale of the current colormap
 %
-% DATA_MARKER: Use Matlab Marker specifier for data points ('o' default)
+% DATA_MARKER: Matlab Marker specifier for data points ('o' default)
 %
 % DATA_MARKER_SIZE: Size of markers used for data points
 %
-% AVE_MARKER: Use Matlab Marker specifier for average points ('o' default)
+% AVE_MARKER: Matlab Marker specifier for average points ('o' default)
 %
 % AVE_MARKER_SIZE: Size of markers used for average points
 %
-% DELETE_OUTLIERS: Delete outliers using Grubb's test, as implemented by
-%   Brett Shoelson (www.mathworks.com/matlabcentral/fileexchange/3961-deleteoutliers)
-%   Provide 2 element array containing alpha and rep: e.g. [.05 1]
+% DELETE_OUTLIERS: Two element array containing alpha and rep: e.g. [.05 1]
+%   or 'No' (Default). Delete outliers using Grubb's test, as implemented
+%   by Brett Shoelson
+%   (www.mathworks.com/matlabcentral/fileexchange/3961-deleteoutliers)
 %
 % PLOT_SDS: Plot horizontal lines corresponding to the # of standard
 %   deviations of the data in the first column. e.g. 2
@@ -67,20 +68,20 @@ function errorbarjitter(data,h,varargin)
 % SAVE_DIRECT: Save figure directly to specified format without plotting in
 %   Matlab window. To use, specify output filename, which must include
 %   legitimate suffix for Matlab saveas command. e.g. 'plot.png'. NOTE:
-%   If you save in Matlab .fig format (i.e. 'plot.fig'), the figure is saved in 
+%   If you save in Matlab .fig format (i.e. 'plot.fig'), the figure is saved in
 %   'invisible' mode. You can open the plot in visible mode with
 %   openfig('plot.fig','new','visible') or, after opening, set(gcf,'Visible','on')
 %
 % ACKNOWLEDGEMENT: This function depends on jitter.m, written by Richie
 %   Cotton 2006/03/21.
-% 
+%
 %
 %
 % $	Author: David Stern	$   $   Date :2011/11/02   $
 %
 % Bug Fixes and Improvements
 % 2012/09/27    Added ability to plot one column of data.
-% 2012/10/16    Fixed bug that caused crash when tried to sort multiple 
+% 2012/10/16    Fixed bug that caused crash when tried to sort multiple
 %               samples with the same mean
 %               Add ability to plot error bars on each sample
 %               Added ability to plot different groups with different
@@ -89,7 +90,7 @@ function errorbarjitter(data,h,varargin)
 %               options missing.
 % 2012/10/27    Added option to save figure directly without plotting
 % 2012/10/30    Changed input format to handle many variables
-%               Control colors of point, averages, and lines separately 
+%               Control colors of point, averages, and lines separately
 %               Plot using regular Matlab symbols
 % 2012/12/10    Added ability to define average and data marker sizes
 % 2012/12/12    Plot with tight X axis
@@ -99,6 +100,10 @@ function errorbarjitter(data,h,varargin)
 % 2013/12/20    Added option to specify matrix of values for datapoint
 %               colors & option to plot without average±SD
 % 2014/1/8      Small bug fix of marker size for plots with colormap
+% 2014/5/7      Small bug fix when no use of color_map
+% 2014/5/8      Improved instructions
+% 2014/5/14     Now clears figure before replotting and correctly colors
+%               bar ends when this option chosen
 
 
 
@@ -242,13 +247,14 @@ end
 %make figure
 
 %figure(h)
+clf(h)
 hold on
 
 %add option to sort data by mean
 %there must be an easier way to rearrange an array by a property of columns
 if strcmp(sort_or_nosort,'sort') == 1
     
-    [sorted_means,sort_idx] = sort(mean_d);    
+    [sorted_means,sort_idx] = sort(mean_d);
     
     %now put original array in new order
     %if data in colheaders, sort that one too
@@ -263,7 +269,7 @@ if strcmp(sort_or_nosort,'sort') == 1
         sorted_std = std(:,sort_idx);
         std = sorted_std;
     end
-
+    
     
     if skip_colheaders ~= 1
         sorted_colheaders = colheaders(sort_idx);
@@ -298,7 +304,7 @@ x = 1:1:n_categories;
 x = x*Xsep;
 
 %put column indices in each column
-    
+
 %plot mean and error bar
 if ~strcmp(average,'none')
     if strcmp(barends,'no') == 1
@@ -320,10 +326,10 @@ if ~strcmp(average,'none')
     else %draw with barends
         if color_opts(2) == 0
             if color_opts(3) == 0
-                errorbar(mean_d,e,ave_marker,'MarkerFaceColor','k','MarkerEdgeColor','k','XData',x+offset);
+                errorbar(mean_d,e,[ave_marker 'k'],'MarkerFaceColor','k','MarkerEdgeColor','k','XData',x+offset);
             else
                 for i = 1:n_categories
-                    errorbar(mean_d(i),e(i),ave_marker,'MarkerFaceColor','k','MarkerEdgeColor','k','Color',colors{i},'XData',x(i)+offset);
+                    errorbar(mean_d(i),e(i),[ave_marker 'k'],'MarkerFaceColor','k','MarkerEdgeColor','k','Color',colors{i},'XData',x(i)+offset);
                 end
             end
         else
@@ -362,7 +368,7 @@ end
 x(isnan(d)) = NaN;
 
 for i = 1:n_categories
-    if ismatrix(color_map)
+    if ~strcmp(color_map,'no')
         scatter(x(:,i)-offset,d(:,i),data_marker_size,color_map(:,i),'marker',data_marker)
     else
         if color_opts(1) == 1
@@ -378,7 +384,7 @@ end
 if ~isempty(std)
     for i = 1:n_categories
         for j = 1:n_data
-            if ismatrix(color_map)
+            if ~strcmp(color_map,'no')
                 if ~isnan(color_map(j,i))
                     coloridx = ceil(max(color_map(j,i) * 64)); %get colormap value of datum
                     line([x(j,i)-offset x(j,i)-offset],[d(j,i)-std(j,i) d(j,i)+std(j,i)],'Color',cmap(coloridx,:),'LineWidth',.5)
@@ -403,7 +409,7 @@ if skip_colheaders == 0
     %add x axis labels
     set(gca,'XTickLabel',colheaders)
 else
-    set(gca,'XTickLabel',[])    
+    set(gca,'XTickLabel',[])
 end
 hold off
 if ~isempty(save_direct);
@@ -415,26 +421,26 @@ function y = jitter(x, factor, uniformOrGaussianFlag, smallOrRangeFlag, realOrIm
 % noise can be uniformly or normally distributed, and can have a magnitude
 % based upon the range of values of X, or based upon the smallest
 % difference between values of X (excluding 'fuzz').
-% 
+%
 % NOTE: This function accepts complex values for the first input, X.  If
 % any values of X have imaginary components (even zero-valued imaginary
 % components), then by default the noise will be imaginary.  Otherwise, the
 % default is for real noise.  You can choose between real and imaginary
 % noise by setting the fifth input parameter (see below).
-% 
+%
 % Y = JITTER(X) adds an amount of uniform noise to the input X, with a
 % magnitude of one fifth of the smallest difference between X values
 % (excluding 'fuzz'), i.e. the noise, n~U(-d/5, d/5), where d is the
 % smallest difference between X values.
-% 
+%
 % Y = JITTER(X, FACTOR) adds noise as above, but scaled by a factor
 % of FACTOR, i.e. n~U(-FACTOR*d/5, FACTOR*d/5).
-% 
+%
 % Y = JITTER(X, FACTOR, 1) adds noise as above, but normally distributed
 % (white noise), i.e. n~N(0, FACTOR*d/5). JITTER(X, FACTOR, 0) works the
 % same as JITTER(X, FACTOR). If the second parameter is left empty (for
 % example JITTER(X, [], 1)), then a default scale factor of 1 is used.
-% 
+%
 % Y = JITTER(X, FACTOR, [], 1) adds an amount of noise to X with a
 % magnitude of one fiftieth of the range of X.  JITTER(X, FACTOR, [], 0)
 % works the same as JITTER(X, FACTOR, []).  A value of 0 or 1 can be given as
@@ -442,7 +448,7 @@ function y = jitter(x, factor, uniformOrGaussianFlag, smallOrRangeFlag, realOrIm
 % i.e. n~U(-FACTOR*r/50, FACTOR*r/50) OR n~N(0, FACTOR*r/50), where r is
 % the range of the values of X.  If the second parameter is left empty then
 % a default scale factor of 1 is used.
-% 
+%
 % Y = JITTER(X, FACTOR, [], [], 1) adds an amount of noise as above, but
 % with imaginary noise.  The magnitude of the noise is the same as in the
 % real case, but the phase angle is a uniform random variable, theta~U(0,
@@ -452,35 +458,35 @@ function y = jitter(x, factor, uniformOrGaussianFlag, smallOrRangeFlag, realOrIm
 % the fourth input to choose between using the smallest distance between
 % values or the range for determining the magnitude of the noise.  If the
 % second parameter is left empty then a default scale factor of 1 is used.
-% 
-% 
+%
+%
 % EXAMPLE:  x = [1 -2 7; Inf 3.5 NaN; -Inf 0.001 3];
 %           jitter(x)
-% 
+%
 %           ans =
-% 
+%
 %             0.9273   -2.0602    6.9569
 %                Inf    3.4597       NaN
 %               -Inf    0.0333    2.9130
-% 
-%           %Plot a noisy sine curve. 
+%
+%           %Plot a noisy sine curve.
 %           x2 = sin(0:0.1:6);
-%           plot(jitter(x2, [], 1, 1));  
-% 
-% 
+%           plot(jitter(x2, [], 1, 1));
+%
+%
 % ACKNOWLEGEMENT: This function is based upon the R function of the same
 % name, written by Werner Stahel and Martin Maechler, ETH Zurich.
 % See http://stat.ethz.ch/R-manual/R-patched/library/base/html/jitter.html
 % for details of the original.
-% 
-% 
+%
+%
 %   Class support for input X:
 %      float: double, single
-% 
-% 
+%
+%
 %   See also RAND, RANDN.
-% 
-% 
+%
+%
 % $ Author: Richie Cotton $     $ Date: 2006/03/21 $
 
 
@@ -488,7 +494,7 @@ function y = jitter(x, factor, uniformOrGaussianFlag, smallOrRangeFlag, realOrIm
 if nargin < 1
     error('jitter:notEnoughInputs', 'This function requires at least one input.');
 end
-    
+
 % Set defaults where required
 if nargin < 2 || isempty(factor)
     factor = 1;
@@ -520,7 +526,7 @@ if ~smallOrRangeFlag
     xDifferences = diff(xUnique);
     if length(xDifferences)
         smallestDistance = min(xDifferences);
-    elseif xUnique ~= 0 
+    elseif xUnique ~= 0
         % In this case, all values are the same, so xUnique has length 1
         smallestDistance = 0.1 * xUnique;
     else
